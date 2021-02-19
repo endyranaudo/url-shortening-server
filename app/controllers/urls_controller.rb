0e4248637170e9ad32c9e64585700731f4648c06
Endy Ranaudo <endy@wearesnook.com>
2021-02-19 13:38:17 +0000

    - Added show method and updated `route.rb` - Added test for `#show` method - Added `CORS` - Added `.gitignore`

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,4 +5,15 @@ class UrlsController < ApplicationController
     render json: urls
   end
 
+  def show
+    url = Url.find_by(shortened: url_params[:shortened])
+    redirect_to url.original
+  end
+
+
+  private 
+
+  def url_params
+    params.permit(:original, :shortened)
+  end
 end
--- spec/controllers/urls_request_spec.rb
+++ spec/controllers/urls_request_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
-# RSpec.describe "Urls", type: :request do
-# end
+RSpec.describe "Urls", type: :request do
+  subject { response }
+
+  describe "GET urls#index" do
+    it "should get index" do
+      get '/urls'
+      expect(subject).to have_http_status(200)
+    end
+  end
+
+  describe '#show' do
+    let(:url) { Url.create!({original: 'http://bionic.co.uk', shortened: '123456'})}
+    
+    it 'redirects to the original URL' do
+      get "/#{url.shortened}"
+      expect(subject).to redirect_to(url.original)
+    end
+  end
+end
