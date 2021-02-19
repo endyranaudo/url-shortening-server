172a5614426daf31c027b3bf80d71bac8c881995
Endy Ranaudo <endy@wearesnook.com>
2021-02-19 16:56:08 +0000

    - Added `README.md` with details and instructions - Added `#create` method - no test - Created `.shorten` method that generates a random shortened number

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -10,10 +10,32 @@ class UrlsController < ApplicationController
     redirect_to url.original
   end
 
+  def create
+    if !url_params[:original].include?("http")
+      original = "http://#{url_params[:original]}"
+    else
+      original = url_params[:original]
+    end
+    
+    if Url.find_by(original: original)
+      render json: Url.find_by(original: original)
+    else
+      url = Url.new(original: original)
+      if url.save
+        url.shortened = Url.shorten
+        url.save
+        render json: url
+      else
+        render json: {error: "'#{url_params[:original]}' is not a valid URL"}
+      end
+    end
+  end
+
 
   private 
 
   def url_params
     params.permit(:original, :shortened)
   end
+
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,9 @@
 class Url < ApplicationRecord
   validates :original, presence: true
+
+  def self.shorten
+    random = (0...6).map { (65 + rand(26)).chr }.join.downcase
+    "#{random}"
+  end
+
 end
